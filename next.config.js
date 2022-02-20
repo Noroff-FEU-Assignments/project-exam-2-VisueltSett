7efa251ffb4618117b5304f5789858c541c55749
VisueltSett_AneTh <ane@visueltsett.no>
2022-02-20 14:20:23 +0100

    commiting changes dompurify

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [
       'https://www.osebroantikvariat.no',
-      'http://localhost:8080/osebroantikvariat',
+      'https://visueltsett.no/osebroantikvariat',
     ],
   },
   env: {
-    NEXT_APP_BASE_URL: 'http://localhost:8080/osebroantikvariat',
+    NEXT_APP_BASE_URL: 'https://visueltsett.no/osebroantikvariat',
     WOOCOMMERCE_KEY_Ane: 'ck_53bbd3d011773c47bb456e8637948debaa7c6d20',
     WOOCOMMERCE_SECRET_Ane: 'cs_fac2cb14c057515b6f29f974e9d8246380d12deb',
     WOOCOMMERCE_KEY_OSEBRO_ADMIN: 'ck_5a953325e39a0fe4ae44e9d6f8052466ba664569',
