46d3d6ba75b6e59e4a622533e2b27b05ee074dc1
VisueltSett_AneTh <ane@visueltsett.no>
2022-03-09 20:13:47 +0100

    commit subdomain path

--- next.config.js
+++ next.config.js
@@ -8,10 +8,11 @@ const nextConfig = {
     domains: [
       'https://www.osebroantikvariat.no',
       'https://detnorskewebselskab.no',
+      'https://wp.detnorskewebselskab.no',
     ],
   },
   env: {
-    NEXT_APP_BASE_URL: 'https://www.detnorskewebselskab.no',
+    NEXT_APP_BASE_URL: 'https://wp.detnorskewebselskab.no',
     WOOCOMMERCE_KEY_Ane: 'ck_53bbd3d011773c47bb456e8637948debaa7c6d20',
     WOOCOMMERCE_SECRET_Ane: 'cs_fac2cb14c057515b6f29f974e9d8246380d12deb',
     WOOCOMMERCE_KEY_OSEBRO_ADMIN: 'ck_5a953325e39a0fe4ae44e9d6f8052466ba664569',
